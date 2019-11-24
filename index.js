db52874679d9ebab8d15ac6b75ff741dcc1231fa
hs-yonekura <y-yonekura@science.co.jp>
2019-11-24 15:07:07 +0900

    config function added

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ module.exports = {
         const config   = book.config.get('pluginsConfig.font-size', {});
         var fontSize = "140";
         if(config.fontsize !== undefined) fontSize = config.fontsize;
-
         return '<span style="font-size:'  + String(fontSize) + '%">' content.body + '</span>';
       }
     }
   }
 }
+
